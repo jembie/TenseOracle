92fb448e7058ea1c5e53f9ff6dd9064f8acea621
jembie <75253705+jembie@users.noreply.github.com>
2024-09-16 12:18:06 +0200

    Add `sklearn_filters.py` in `Strategies/`

--- Strategies/__init__.py
+++ Strategies/__init__.py
@@ -1,5 +1,30 @@
+from Strategies.AE_filters import (
+    AutoFilter_Chen_Like,
+    AutoFilter_LSTM,
+    AutoFilter_LSTM_SIMPLE,
+)
+from Strategies.dsm_filters import (
+    LoserFilter_Optimized_Pseudo_Labels,
+    LoserFilter_Plain,
+    LoserFilter_SSL_Variety,
+)
 from Strategies.filters import RandomFilter
-from Strategies.dsm_filters import LoserFilter_SSL_Variety, LoserFilter_Plain, LoserFilter_Optimized_Pseudo_Labels
-from Strategies.AE_filters import AutoFilter_Chen_Like, AutoFilter_LSTM, AutoFilter_LSTM_SIMPLE
+from Strategies.LE_filters import (
+    TeachingFilter,
+    TeachingFilter_Smooth,
+    TeachingFilter_WOW,
+)
 from Strategies.other_filters import SingleStepEntropy, SingleStepEntropy_SimplePseudo
-from Strategies.LE_filters import TeachingFilter, TeachingFilter_Smooth, TeachingFilter_WOW
+from Strategies.sklean_filters import IsolationForestFilter, LocalOutlierFactorFilter, HDBScanFilter
+
+# ======= Only use these filters for experiments =======
+
+# Create synonymous meaning for filters
+SimpleDSM = LoserFilter_Plain
+
+# LSTM = Long Short Term Memory, AE = Auto Encoder
+SyntacticAE = AutoFilter_LSTM_SIMPLE  # DO NOT USE FOR EVALUATION
+
+SemanticAE = AutoFilter_Chen_Like
+
+SimpleSS = SingleStepEntropy_SimplePseudo
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import sys
-import time
 
 from Utilities.parsers import parse_config, parse_args, parse_task_config
 from Utilities.comet import CometExperiment
@@ -7,7 +6,7 @@ from Utilities.preprocessing import load_dataset_from_config
 from Utilities.active_learning import load_model, load_query_strategy, load_active_learner, initialize_active_learner
 from Utilities.active_learning import perform_active_learning
 from Utilities.evaluation import assess_dataset_quality
-import copy
+
 import torch
 import numpy as np
 from Utilities.general import set_random_seed, log_failed_attempts
@@ -20,11 +19,9 @@ def main():
     num_classes = len(set(train.y).union(test.y))
     clf_factory = load_model(config, num_classes)
 
-    query_strategy = load_query_strategy(strategy_name=args.strategy_name,
-                                         filter_name=args.filter_strategy_name,
-                                         config=config,
-                                         args=args,
-                                         num_classes=num_classes)
+    query_strategy = load_query_strategy(
+        strategy_name=args.strategy_name, filter_name=args.filter_strategy_name, config=config, args=args, num_classes=num_classes
+    )
 
     # Init Learner & Seed Set
     active_learner = load_active_learner(clf_factory, query_strategy, train)
@@ -55,10 +52,7 @@ def main():
         experiment=experiment,
     )
 
-    metrics_to_log = {
-        "avg_duration": sum(tt := active_learner.query_strategy.time_tracker)/len(tt),
-        **set_performance
-    }
+    metrics_to_log = {"avg_duration": sum(tt := active_learner.query_strategy.time_tracker) / len(tt), **set_performance}
 
     experiment.log_metrics(metrics_to_log)
     experiment.log_results(np.array(active_learner.query_strategy.time_tracker), "durations")
@@ -66,7 +60,7 @@ def main():
     return
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parse_args()
     config = parse_config(args)
     task_config = parse_task_config(args)
@@ -82,7 +76,7 @@ if __name__ == '__main__':
             print(torch.version.cuda)
             if torch.cuda.device_count() == 0:
                 log_failed_attempts(args.random_seed, args.task_config, args.filter_strategy_name, config["SHARED_CACHE_ADR"])
-                raise Exception("No GPU Found, If none required please set --gpu_optional")
+            raise Exception("No GPU Found, If none required please set --gpu_optional")
 
     main()
     sys.exit(0)
