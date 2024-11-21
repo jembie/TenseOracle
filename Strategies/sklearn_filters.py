f71443ea1e5f5096889da3e87051db405f07ac01
toma076c <toma076c@login1.alpha.hpc.tu-dresden.de>
2024-11-21 11:42:54 +0100

    Update `Strategies/sklearn_filters.py`

--- Strategies/sklearn_filters.py
+++ Strategies/sklearn_filters.py
@@ -12,18 +12,8 @@ from Strategies.filters import FilterStrategy
 
 
 def detect_outliers(filter_strategy: FilterStrategy, train_data: ndarray, outliers_to_check: ndarray) -> ndarray:
-    outlier_classifier = filter_strategy.fit(train_data)
-
-    score_samples = outlier_classifier.decision_function(train_data)
-    mean = np.mean(score_samples)
-    std = np.std(score_samples)
-
-    threshold = (
-        mean - 2 * std
-    )  # The lower, the more abnormal. Negative scores represent outliers, positive scores represent inliers. That's why mean - 2 * std instead of mean + 2 * std
-
-    boolean_mask = outlier_classifier.decision_function(outliers_to_check) < threshold
-
+    prediction = filter_strategy.fit_predict(train_data)
+    boolean_mask = (prediction[outliers_to_check]) == -1
     return boolean_mask
 
 
@@ -47,13 +37,10 @@ class IsolationForestFilter(FilterStrategy):
         n=10,
         iteration=0,
     ) -> ndarray:
-        outliers_to_check = embeddings[indices_chosen]
-        # train_data = np.delete(embeddings, indices_chosen)
-
         boolean_mask = detect_outliers(
-            filter_strategy=IsolationForest(n_estimators=400, random_state=self.seed, max_samples=len(embeddings)),
+            filter_strategy=IsolationForest(n_estimators=400, random_state=self.seed, max_samples=len(embeddings), n_jobs=-1),
             train_data=embeddings,
-            outliers_to_check=outliers_to_check,
+            outliers_to_check=indices_chosen,
         )
 
         return boolean_mask
@@ -79,13 +66,11 @@ class LocalOutlierFactorFilter(FilterStrategy):
         n=10,
         iteration=0,
     ) -> ndarray:
-        outliers_to_check = embeddings[indices_chosen]
-        train_data = np.delete(embeddings, indices_chosen, axis=0)
-
         boolean_mask = detect_outliers(
-            filter_strategy=LocalOutlierFactor(metric="cosine", novelty=True), train_data=train_data, outliers_to_check=outliers_to_check
+            filter_strategy=LocalOutlierFactor(metric="cosine", novelty=False, n_jobs=-1),
+            train_data=embeddings, 
+            outliers_to_check=indices_chosen
         )
-
         return boolean_mask
 
 
@@ -109,9 +94,8 @@ class HDBScanFilter(FilterStrategy):
         n=10,
         iteration=0,
     ) -> ndarray:
-        hdb = HDBSCAN(metric="cosine")
+        hdb = HDBSCAN(metric="cosine", n_jobs=-1)
         hdb.fit(embeddings)
-        labels = hdb.labels_ < 0
-        print(f"Total Outliers detected for: HDBScan {sum(labels) / len(labels)}")
+        labels = hdb.labels_ == -1
 
         return labels[indices_chosen]
