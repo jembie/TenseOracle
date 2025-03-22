333d34744f59edde1d49f5eab2ac639f9a7fb7ed
jembie <75253705+jembie@users.noreply.github.com>
2025-03-22 17:26:03 +0100

    Remove __init__.py

--- ThoroughOracle-Scripts/__innit__.py
+++
@@ -1,6 +0,0 @@
-from .comet_downloader import *
-from .constants import *
-from .endperformance_analysis import *
-from .missed_seeds import *
-from .significance_test import *
-from .standard_analysis import *
