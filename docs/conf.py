f715087e9b8f7b4a7fd37dc489221b1c43b17458
jembie <75253705+jembie@users.noreply.github.com>
2025-03-23 16:52:16 +0100

    Add Icon to Browser Tab

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ html_theme = "pydata_sphinx_theme"
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
 html_logo = "_images/db_chair_logo.svg"
-
+html_favicon = "_images/db_chair_logo.svg"
 
 html_theme_options = {
     "logo": {
