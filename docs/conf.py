1a9157cc4b18a56efbcb3fca19a14357f28e2215
jembie <75253705+jembie@users.noreply.github.com>
2025-04-05 13:45:47 +0200

    Update CSS icons for admonitions and reduce primary (left) sidebar length from 25% -> 20% for easier reading.

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@
 
 project = "TenseOracle"
 copyright = "2025, Jimmy Pöhlmann"
-author = "Jimmy Pöhlmann"
+author = "jembie"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
